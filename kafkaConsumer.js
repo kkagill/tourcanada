6ac1bff5e3c0b4feabce12be81c23e43889d2236
Nam Giang <kyng@ece.ubc.ca>
2015-07-03 07:59:49 +0000

    adding offset save

--- kafkaConsumer.js
+++ kafkaConsumer.js
@@ -2,7 +2,8 @@ var config = require('./config'),
     kafka = require('kafka-node'),
     Consumer = kafka.Consumer,
     client = new kafka.Client(config.ZOOKEEPER),
-    Q = require('q');
+    Q = require('q'),
+    fs = require('fs');
 
 var options = {
     autoCommit: true,
@@ -15,6 +16,34 @@ var consumerReady = false;
 
 consumer.on('error', function (err) {console.log('consumer error: ' + err)});
 
+consumer.on('ready', function (){
+   console.log('consumer ready!');
+});
+
 consumer.on('message', function (message) {
     console.log(message);
 });
+
+function saveOffset(offset){
+    fs.writeFile("offsetTracker.kafka", offset, function(err) {
+        if(err) {
+            console.log(err);
+        }
+    }); 
+}
+
+function retrieveOffset(){
+    var d = Q>defer();
+    
+    fs.readFile('offsetTracker.kafka', 'utf8', function (err,data) {
+        if (err) {
+            console.log(err);
+            d.reject(new Error('Error retrieving offset'));
+        } else if (isNaN(data)) {
+            d.reject(new Error('Error reading offset'));
+        } else 
+            d.resolve(data);
+    });
+    
+    return d;
+}
