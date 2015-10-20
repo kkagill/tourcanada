8215361253f5156b9c3e89f3dcf3e26786fa9055
kkagill <kkagill@gmail.com>
2015-10-19 17:21:28 -0700

    milestone 1

--- public/app/scripts/controllers/users.js
+++ public/app/scripts/controllers/users.js
@@ -7,88 +7,100 @@
  * # TripdetailCtrl
  * Controller of the keeboo
  */
-angular.module('keeboo')
-  .controller('UsersCtrl', function ($scope) {
-    
-    var mapOptions = {
-            zoom: 4,
-            center: new google.maps.LatLng(40.0000, -98.0000)
-    };
-    
-    $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+ angular.module('keeboo')
+     .controller('UsersCtrl', function($scope) {
 
-    var trip = function (id, name, desc, avgEcon, distance, date){
-        this.name = name;
-        this.id = id;
-        this.date = date;
-        this.desc = desc;
-        this.avgEcon = avgEcon;
-        this.distance = distance;
-    };
-    
-    $scope.trip = new trip(1, "Vancouver Trip", "This is our Vancouver trip", "10.1L/100Km", 12, null);
+         //same marker clusterer car icon for different zoom levels
+         var styles = [{
+             url: '../images/car.png',
+             height: 30,
+             width: 30,
+             textColor: 'orange',
+             textSize: 20
+         }, {
+             url: '../images/car.png',
+             height: 30,
+             width: 30,
+             textColor: 'orange',
+             textSize: 20
+         }, {
+             url: '../images/car.png',
+             height: 30,
+             width: 30,
+             textColor: 'orange',
+             textSize: 20
+         }];
 
-    var cities = [
-        {
-            city : 'Toronto',
-            desc : 'This is the best city in the world!',
-            lat : 43.7000,
-            long : -79.4000
-        },
-        {
-            city : 'New York',
-            desc : 'This city is aiiiiite!',
-            lat : 40.6700,
-            long : -73.9400
-        },
-        {
-            city : 'Chicago',
-            desc : 'This is the second best city in the world!',
-            lat : 41.8819,
-            long : -87.6278
-        },
-        {
-            city : 'Los Angeles',
-            desc : 'This city is live!',
-            lat : 34.0500,
-            long : -118.2500
-        },
-        {
-            city : 'Las Vegas',
-            desc : 'Sin City...\'nuff said!',
-            lat : 36.0800,
-            long : -115.1522
-        }
-    ];
-    
-    $scope.markers = [];
-    
-    var infoWindow = new google.maps.InfoWindow();
-    
-    var createMarker = function (info){
-        
-        var marker = new google.maps.Marker({
-            map: $scope.map,
-            position: new google.maps.LatLng(info.lat, info.long),
-            title: info.city
-        });
-        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-        
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-            infoWindow.open($scope.map, marker);
-        });
-        
-        $scope.markers.push(marker);
-    }
-    
-    for (var i = 0; i < cities.length; i++){
-        createMarker(cities[i]);
-    }
+         //Vancouver coordinates for drawing polygon
+         var vancouverCoordinates = [
+           {lat: 49.221942, lng: -123.023572},
+           {lat: 49.293225, lng: -123.023186},
+           {lat: 49.295968, lng: -123.136225},
+           {lat: 49.276037, lng: -123.214674},
+           {lat: 49.276037, lng: -123.214674},
+           {lat: 49.258451, lng: -123.215446},
+           {lat: 49.258002, lng: -123.197079},
+           {lat: 49.234581, lng: -123.196821},
+           {lat: 49.202794, lng: -123.134251},
+           {lat: 49.210981, lng: -123.101807},
+           {lat: 49.225221, lng: -123.101635},
+           {lat: 49.22197,  lng: -123.023529}
+         ];
 
-    $scope.openInfoWindow = function(e, selectedMarker){
-        e.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
-    }
-    
-  });
+         var mapOptions = {
+             zoom: 11,
+             styles: styles,
+             center: new google.maps.LatLng(49.2648809977945, -123.01776962890625),
+             mapTypeId: google.maps.MapTypeId.TERRAIN
+         };
+
+         $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+         $scope.markers = [];
+
+         var infoWindow = new google.maps.InfoWindow();
+
+         var createMarker = function(info) {
+
+             var marker = new google.maps.Marker({
+                 map: $scope.map,
+                 position: new google.maps.LatLng(info.lat, info.long),
+                 //car icon for a individual marker
+                 icon: 'http://icons.iconarchive.com/icons/iconshock/real-vista-transportation/32/vintage-car-icon.png'
+             });
+
+             marker.content = '<div class="infoWindowContent">' + info.location + '</div>';
+
+             google.maps.event.addListener(marker, 'click', function() {
+                 infoWindow.setContent('<h2>' + info.number + '</h2>' + marker.content);
+                 infoWindow.open($scope.map, marker);
+             });
+
+             $scope.markers.push(marker);
+         }
+
+         for (var i = 0; i < coordinates.length; i++) {
+             createMarker(coordinates[i]);
+         }
+
+         // Start Marker Clusterer
+         $scope.markerCluster = new MarkerClusterer($scope.map, $scope.markers, mapOptions);
+
+          // Construct the polygon.
+          var vancouver = new google.maps.Polygon({
+            paths: vancouverCoordinates,
+            strokeColor: '#18FC4D',
+            strokeOpacity: 0.6,
+            strokeWeight: 3,
+            fillColor: '#18FC4D',
+            fillOpacity: 0.10
+          });
+          vancouver.setMap($scope.map);
+
+         $scope.openInfoWindow = function(e, selectedMarker) {
+             e.preventDefault();
+             google.maps.event.trigger(selectedMarker, 'click');
+         }
+
+
+     });
